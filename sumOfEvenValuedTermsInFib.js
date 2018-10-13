bce0c53e9928125c7be900a3eae99c0bb244fc0e
cksharma11 <cksharma122@gmail.com>
2018-10-13 09:42:31 +0530

    Update sumOfEvenValuedTermsInFib.js

--- sumOfEvenValuedTermsInFib.js
+++ sumOfEvenValuedTermsInFib.js
@@ -1,10 +1,10 @@
-const findSumOfEvenValues = function(nthTerm){
+const findSumOfEvenValues = function(limitOfValue){
   let sum = 2;
   let firstTerm = 0;
   let secondTerm = 2
   let nextTerm = 0;
 
-  while(nextTerm < nthTerm){
+  while(nextTerm < limitOfValue){
     sum += nextTerm;
     nextTerm = (4 * secondTerm)+firstTerm;
 
